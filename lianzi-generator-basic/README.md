dc6e33122818e748ab7f74e735f75c4af82ce399
Menghao Zhao <zmh1084339069@163.com>
2024-06-27 22:53:54 +0800

    动静文件生成目录获取方式调整

---
+++ lianzi-generator-basic/README.md
@@ -0,0 +1,7 @@
+## 修改点
+```
+System.getProperty("user.dir");
+```
+在 idea 中该行代码默认打开工程的根目录，
+举个例子，在 `idea` 中打开 `lianzi-generator` 项目则获取到的根路径是 `lianzi-generator`，
+如果打开子项目 `lianzi-generator-basic` 则获取到的根路径是 `lianzi-generator-basic`
--- lianzi-generator-basic/src/main/java/com/fulian/generator/MainGenerator.java
+++ lianzi-generator-basic/src/main/java/com/fulian/generator/MainGenerator.java
@@ -7,27 +7,37 @@ import java.io.File;
 import java.io.IOException;
 
 public class MainGenerator {
-    public static void main(String[] args) throws TemplateException, IOException {
-
-        // 1. 静态文件生成
+    /**
+     *
+     * @param model 数据模型
+     * @throws TemplateException
+     * @throws IOException
+     */
+    public static void doGenerate(Object model) throws TemplateException, IOException {
         String projectPath = System.getProperty("user.dir");
+        // 整个项目的根路径
+        File parentFile = new File(projectPath).getParentFile();
+        System.out.println(parentFile.getName());
         // 输入路径
-        String inputPath = projectPath + File.separator + "lianzi-generator-demo-projects" + File.separator + "acm-template";
+        String inputPath = new File(parentFile,"lianzi-generator-demo-projects/acm-template").getAbsolutePath();
         // 输出路径
         String outputPath = projectPath;
-        // 复制
+        // 生成静态文件
         StaticGenerator.copyFilesByRecursive(inputPath,outputPath);
 
-        // 动态文件生成
-        String dynamicInputPath = projectPath + File.separator + "lianzi-generator-basic" + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        // 生成动态文件
+        String dynamicInputPath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
         String dynamicoutputPath = projectPath + File.separator + "acm-template/src/com/fulian/acm/MainTemplate.java";
+        DynamicGenerator.doGenerate(dynamicInputPath,dynamicoutputPath,model);
+    }
+
+    public static void main(String[] args) throws TemplateException, IOException {
 
         // 数据模型
         MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
         mainTemplateConfig.setAuthor("fulian");
         mainTemplateConfig.setOutputText("求和结果");
         mainTemplateConfig.setLoop(false);
-
-        DynamicGenerator.doGenerate(dynamicInputPath,dynamicoutputPath,mainTemplateConfig);
+        doGenerate(mainTemplateConfig);
     }
 }
